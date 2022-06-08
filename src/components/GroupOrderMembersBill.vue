41ed5864ab862cb9f52bfad5fd19490dd88311f1
Vincent Blouin <vincent.blouin@gmail.com>
2022-06-08 15:48:18 -0400

    list user orders

--- src/components/GroupOrderMembersBill.vue
+++ src/components/GroupOrderMembersBill.vue
@@ -6,20 +6,39 @@
     <v-card-text v-if="!isLoading && !userOrders.length">
       {{ $t('membersBill:noBills') }}
     </v-card-text>
-    <v-card-text v-if="!isLoading && userOrders.length">
-      <v-list>
-        <v-list-item v-for="userOrder in userOrders" :key="userOrder.id">
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ userOrder.User.firstname }}
-              {{ userOrder.User.lastname }}
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              {{ userOrder.totalPrice | currency }}
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+    <v-card-text v-if="!isLoading && userOrders.length" class="vh-center">
+      <v-row>
+        <v-col cols="12">
+          <v-btn class="mt-4">
+            <v-icon left>file_download</v-icon>
+            {{ $t('membersBill:downloadAll') }}
+          </v-btn>
+        </v-col>
+        <v-col cols="12" class="vh-center">
+          <v-list width="375">
+            <div v-for="userOrder in userOrders" :key="userOrder.id">
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon @click="downloadReceipt">file_download</v-icon>
+                </v-list-item-action>
+                <v-list-item-action>
+                  <v-icon @click="viewReceipt">receipt</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ userOrder.User.firstname }}
+                    {{ userOrder.User.lastname }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{ userOrder.totalPrice | currency }}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider></v-divider>
+            </div>
+          </v-list>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
@@ -33,10 +52,12 @@ export default {
   props: ['buyGroupId', 'buyGroupOrderId'],
   data: function () {
     I18n.i18next.addResources("fr", "membersBill", {
-      noBills: "Pas encore de commandes"
+      noBills: "Pas encore de commandes",
+      downloadAll: "Tout télécharger"
     });
     I18n.i18next.addResources("en", "membersBill", {
-      noBills: "Pas encore de commandes"
+      noBills: "Pas encore de commandes",
+      downloadAll: "Tout télécharger"
     });
     return {
       isLoading: true,
@@ -50,6 +71,14 @@ export default {
         this.buyGroupOrderId
     );
     this.isLoading = false;
+  },
+  methods: {
+    downloadReceipt: function () {
+
+    },
+    viewReceipt: function () {
+
+    }
   }
 }
 </script>
